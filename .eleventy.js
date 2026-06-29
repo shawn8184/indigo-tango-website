73459f3cb9523667be9d1ff7d4687d8f857eec0a
Shawn Wallis <shawnwallis@Shawns-MacBook-Pro.local>
2026-06-29 00:33:12 -0400

    Convert site to Eleventy and Pages CMS

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("images");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+};
